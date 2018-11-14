344734c445a781a3c8842f87dc66fc98e2fe1ca6
cristin <cristin.herrera.guerra@gmail.com>
2018-11-14 18:09:33 -0500

    segund solucion

--- db/migrate/20181108150506_addprice_per_night_to_rooms.rb
+++
@@ -1,5 +0,0 @@
-class AddpricePerNightToRooms < ActiveRecord::Migration[5.0]
-  def change
-    add_column :rooms, :price_per_night, :decimal
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181108150506) do
+ActiveRecord::Schema.define(version: 20181114230612) do
 
   create_table "rooms", force: :cascade do |t|
     t.string   "title"
